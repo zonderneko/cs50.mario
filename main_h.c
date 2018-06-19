530f5592955fd03a5c7cd8faeeeec1ce6a92b22f
zonderneko <8taichou8@gmail.com>
2018-06-19 19:51:57 +0300

    Create main_h.c

---
+++ main_h.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,i,j,k,l;
+    printf("Enter int from 0 to 32\n");
+//checking if x in range
+do
+ {
+    scanf("%d", &x);
+    if (x < 1 || x > 32)
+        printf("Retry:\n");
+ }
+    while (x < 1 || x > 32);
+//main pyramide cycle
+for (i = 0; i < x; i++)
+ {
+    for (j = (x - 1 - i); j > 0; j--) //spaces cycle
+        printf(" ");
+    for (k = 0; k < i + 2; k++) //#cycle
+        printf("#");
+    printf(" ");
+    printf(" ");
+    for (l = 0; l < i + 2; l++) //another # cycle
+        printf("#");
+    printf("\n");
+ }
+}

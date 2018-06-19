b5cfb3c863f4d06154b754aa4bdf27de94ac3e66
zonderneko <8taichou8@gmail.com>
2018-06-19 19:31:47 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main()
 {
     int x,i,j,k;
     printf("Enter int from 0 to 32\n");
+//проверка введенного числа
 do
  {
     scanf("%d", &x);
@@ -12,6 +13,7 @@ do
         printf("Retry:\n");
  }
     while (x < 0 || x > 32);
+//основной цикл постройки пирамиды
 for (i = 0; i < x; i++)
  {
     for (j = (x - 1 - i); j > 0; j--)
@@ -19,6 +21,6 @@ for (i = 0; i < x; i++)
     printf("#");
     for (k = 0; k < i + 1; k++)
         printf("#");
-            printf("\n");
+    printf("\n");
  }
 }

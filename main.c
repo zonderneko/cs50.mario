e2df01a248b99f398f5f173271c79eefddde1b52
zonderneko <8taichou8@gmail.com>
2018-06-19 21:03:44 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,22 +3,24 @@
 
 int main()
 {
-    int x,i,j,k;
-    printf("Enter int from 1 to 32\n");
+    int ch,x,i,j,k;
+    printf("Enter int from 0 to 32\n");
 //checking if x in range
 do
  {
-    scanf("%d", &x);
-    if (x < 1 || x > 32)
+    ch = scanf("%d", &x);
+    fflush(stdin);
+    if (x < 0 || x > 32 || ch != 1)
         printf("Retry:\n");
  }
-    while (x < 1 || x > 32);
-//main pyramide cycle
+    while (x < 0 || x > 32 || ch != 1);
+//m
 for (i = 0; i < x; i++)
  {
-    for (j = (x - 1 - i); j > 0; j--) //spaces cycle
+    for (j = (x - 1 - i); j > 0; j--)
         printf(" ");
-    for (k = 0; k < i + 2; k++) //# cycle
+    printf("#");
+    for (k = 0; k < i + 1; k++)
         printf("#");
     printf("\n");
  }
